e1cc464481fbfaa632f4039b6b882807bce0133c
learnityframework <frameworklearnity@gmail.com>
2015-10-30 16:24:33 +0530

    Removed connection closing memory leaks

--- learnitycoreadmin/src/coreadministrationv2/sysmgmt/AssetFileManagement.java
+++ learnitycoreadmin/src/coreadministrationv2/sysmgmt/AssetFileManagement.java
@@ -397,6 +397,11 @@ public class AssetFileManagement extends HttpServlet {
 				form.addElement("<p>Total No. Of Framework Asset: " +grid1.getRows());
 				form.addElement(grid1.getGrid());
 			}	
+			//Added by Diptendu 29-Oct-2015
+			
+			grid1.closeConnection();
+			
+			
 		}
 		catch (Exception exp) {
 			

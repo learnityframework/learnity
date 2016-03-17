be4c26c63f8a9df001804a162c275b181efd41d1
Diptendu Dutta <dutta.diptendu@gmail.com>
2016-03-17 12:46:23 +0530

    Removed the function "numbercheck" from LearnityUtil.js since the function is application specific and does not belong in the framework

--- learnitycoreadmin/WebContent/coreadmin/js/LearnityUtil.js
+++ learnitycoreadmin/WebContent/coreadmin/js/LearnityUtil.js
@@ -216,19 +216,3 @@ function setArrayGridData(gridid,col_id,content)
 	setReloadGrid(gridid);
 }
 
-function numbercheck(value)
-{
-	if(value!=='')
-	{
-		var IntegerCheck = /^-?\d+$/.test(value);
-		if (IntegerCheck===false) 
-		return [false,"Please insert integer value "];
-		else 
-			if(value>24 || value<0)
-				return[false,"Please enter proper value."];
-			else 				
-				return [true,""];
-	}
-	else
-		return [true,""];
-}

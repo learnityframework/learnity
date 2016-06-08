0938cd6f26f0c0addd9a79d23841208dc1c7f0af
diptenduLF <dutta.diptendu@gmail.com>
2016-06-08 12:33:19 +0530

    Added column shrinkToFit to table grid_structure

--- learnitycoreadmin/DB Schema/LearnITyFramework.sql
+++ learnitycoreadmin/DB Schema/LearnITyFramework.sql
@@ -954,6 +954,7 @@ CREATE TABLE `grid_structure` (
   `searchonEnter` varchar(1) DEFAULT NULL,
   `columnChooser` varchar(1) DEFAULT NULL,
   `toolbarSearch` varchar(1) DEFAULT NULL,
+  `shrinkToFit` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`interface_id`,`part_id`),
   KEY `grid_structure1` (`interface_id`,`part_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
